0a55dea747752a3ca33b3fcb16fb27fd10bace92
darkxxx2000 <silvana1002010@gmail.com>
2026-07-20 23:57:33 -0300

    Update home-banner.js

--- js/home-banner.js
+++ js/home-banner.js
@@ -1,1 +1,43 @@
+const slider=document.getElementById("banner-slider");
 
+if(slider){
+
+fetch("data/home-banner.json")
+
+.then(r=>r.json())
+
+.then(images=>{
+
+images.forEach(item=>{
+
+const div=document.createElement("div");
+
+div.className="banner-slide";
+
+div.style.backgroundImage=`url('${item.image}')`;
+
+slider.appendChild(div);
+
+});
+
+let current=0;
+
+setInterval(()=>{
+
+current++;
+
+if(current>=images.length){
+
+current=0;
+
+}
+
+slider.style.transform=`translateX(-${current*100}%)`;
+
+},5000);
+
+})
+
+.catch(console.error);
+
+}
