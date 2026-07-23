fa1acb92f668078ce268f97af527b13a84dcce36
darkxxx2000 <silvana1002010@gmail.com>
2026-07-23 16:21:35 -0300

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,4 +1,3 @@
-```js
 /*************************************************
  * DARKENSHORNS
  * APP
@@ -196,5 +195,3 @@ document.addEventListener(
     "DOMContentLoaded",
     initializeApp
 );
-```
-
